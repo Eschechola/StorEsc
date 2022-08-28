e5b7ac197f66eb21fc7bf321484f4c355b7c5145
Eschechola <lucas.eschechola@outlook.com>
2022-08-28 01:11:43 -0300

    fix: Fix view models validations

---
+++ src/StorEsc.Api/ViewModels/CreateProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorEsc.Api.ViewModels;
+
+public class CreateProductViewModel
+{
+    [Required(ErrorMessage = "Name can not be empty.")]
+    [MinLength(6, ErrorMessage = "Name must be at least 6 characters.")]
+    [MaxLength(200, ErrorMessage = "Name must have a maximum of 200 characters.")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Description can not be empty.")]
+    [MinLength(100, ErrorMessage = "Description must be at least 100 characters.")]
+    [MaxLength(2000, ErrorMessage = "Description must have a maximum of 2000 characters.")]
+    public string Description { get; set; }
+    
+    [Required(ErrorMessage = "Price can not be empty.")]
+    [Range(5, 1000000, ErrorMessage = "Price can be between 5 and 1.000.000")]
+    public decimal Price { get; set; }
+}
--- src/StorEsc.Api/ViewModels/LoginSellerViewModel.cs
+++ src/StorEsc.Api/ViewModels/LoginSellerViewModel.cs
@@ -6,12 +6,12 @@ public class LoginSellerViewModel
 {
     [Required(ErrorMessage = "Email can not be empty.")]
     [MinLength(8, ErrorMessage = "Email must be at least 8 characters.")]
-    [MaxLength(180, ErrorMessage = "Email must have a maximum of 200 characters.")]
+    [MaxLength(200, ErrorMessage = "Email must have a maximum of 200 characters.")]
     [EmailAddress(ErrorMessage = "Email invalid.")]
     public string Email { get; set; }
     
     [Required(ErrorMessage = "Password can not be empty.")]
     [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
-    [MaxLength(180, ErrorMessage = "Email must have a maximum of 120 characters.")]
+    [MaxLength(120, ErrorMessage = "Email must have a maximum of 120 characters.")]
     public string Password { get; set; }
 }
