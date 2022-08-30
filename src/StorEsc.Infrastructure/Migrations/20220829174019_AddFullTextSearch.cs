c262a360727551f7016364bb35b23317b0865877
Eschechola <lucas.eschechola@outlook.com>
2022-08-29 22:48:10 -0300

    feature: Add Full Text Search to SQL Server image

---
+++ src/StorEsc.Infrastructure/Migrations/20220829174019_AddFullTextSearch.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StorEsc.Infrastructure.Migrations
+{
+    public partial class AddFullTextSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT CATALOG StorEscCatalog AS DEFAULT;",
+                suppressTransaction: true);
+
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON [StorEsc].[ste].[Product](Name) KEY INDEX PK_Product;",
+                suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT INDEX ON [StorEsc].[ste].[Product];",
+                suppressTransaction: true);
+            
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT CATALOG StorEscCatalog;",
+                suppressTransaction: true);
+        }
+    }
+}
