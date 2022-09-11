631613de6f6efa12cd948668bb65bfcfefe0bdac
Eschechola <lucas.eschechola@outlook.com>
2022-09-11 01:45:48 -0300

    feature: Add PayRechargeAsync to PaymentDomainService

--- src/StorEsc.DomainServices/Interfaces/IPaymentDomainService.cs
+++ src/StorEsc.DomainServices/Interfaces/IPaymentDomainService.cs
@@ -1,6 +1,8 @@
+using StorEsc.Domain.Entities;
+
 namespace StorEsc.DomainServices.Interfaces;
 
 public interface IPaymentDomainService
 {
-    
+    Task<Payment> PayRecharge(double amount, CreditCard creditCard);
 }
---
+++ src/StorEsc.DomainServices/Services/PaymentDomainService.cs
@@ -0,0 +1,38 @@
+﻿using StorEsc.Domain.Entities;
+using StorEsc.DomainServices.Interfaces;
+using StorEsc.Infrastructure.ExternalServices.Interfaces;
+using StorEsc.Infrastructure.Interfaces.Repositories;
+
+namespace StorEsc.DomainServices.Services;
+
+public class PaymentDomainService : IPaymentDomainService
+{
+    private readonly IPaymentRepository _paymentRepository;
+    private readonly IPaymentExternalService _paymentExternalService;
+    
+    public PaymentDomainService(
+        IPaymentRepository paymentRepository,
+        IPaymentExternalService paymentExternalService)
+    {
+        _paymentRepository = paymentRepository;
+        _paymentExternalService = paymentExternalService;
+    }
+
+    public async Task<Payment> PayRecharge(double amount, CreditCard creditCard)
+    {
+        Payment payment;
+        var paymentResponse = await  _paymentExternalService.PayRecharge(amount, creditCard);
+
+        if (!paymentResponse.IsPaid)
+            payment = new Payment(isPaid: false);
+        else
+            payment = new Payment(
+                hash: paymentResponse.Hash,
+                isPaid: true);
+            
+        _paymentRepository.Create(payment);
+        await _paymentRepository.UnitOfWork.SaveChangesAsync();
+        
+        return payment;
+    }
+}
