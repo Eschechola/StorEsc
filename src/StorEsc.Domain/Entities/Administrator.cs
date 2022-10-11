eb3f772984464d06323357fcd42b5a0edadb1f5b
Eschechola <lucas.eschechola@outlook.com>
2022-10-11 01:56:13 -0300

    feature: Add Administrator entity

--- src/StorEsc.Domain/Entities/Administrator.cs
+++ src/StorEsc.Domain/Entities/Administrator.cs
@@ -1,6 +1,20 @@
 ﻿namespace StorEsc.Domain.Entities;
 
-public class Administrator
+public class Administrator : Account
 {
+    protected Administrator() { }
     
+    public Administrator(
+        Guid walletId,
+        string firstName,
+        string lastName,
+        string email,
+        string password) 
+        : base(walletId, firstName, lastName, email, password)
+    {
+        Validate();
+    }
+
+    public void Valitade()
+        => base.Validate();
 }
