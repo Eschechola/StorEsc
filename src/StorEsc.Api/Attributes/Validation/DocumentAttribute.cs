694eb78f9316d6787e86a4545f02cb31527a06fe
eschechola <lucas.eschechola@outlook.com>
2023-05-12 00:52:22 -0300

    hotfix: add null validation to document

--- src/StorEsc.Api/Attributes/Validation/DocumentAttribute.cs
+++ src/StorEsc.Api/Attributes/Validation/DocumentAttribute.cs
@@ -6,6 +6,9 @@ public class DocumentAttribute: ValidationAttribute
 {
     public override bool IsValid(object value)
     {
+        if (value is null)
+            return false;
+        
         var document = value as string;
 
         if (document.Length == 11 || document.Length == 14)
