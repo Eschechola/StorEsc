1bc7dfb24255d92c4bc19ff48fe05678503446e5
Eschechola <lucas.eschechola@outlook.com>
2023-05-30 16:50:00 -0300

    hotfix: remove unnecessary method signature

--- src/StorEsc.Infrastructure/Interfaces/Repositories/ICustomerRepository.cs
+++ src/StorEsc.Infrastructure/Interfaces/Repositories/ICustomerRepository.cs
@@ -4,7 +4,6 @@ namespace StorEsc.Infrastructure.Interfaces.Repositories;
 
 public interface ICustomerRepository : IRepository<Customer>
 {
-    Task<Customer> GetByIdAsync(string id);
     Task<Customer> GetByEmailAsync(string email, string includeProperties = "");
     Task<bool> ExistsByEmailAsync(string email);
 }
