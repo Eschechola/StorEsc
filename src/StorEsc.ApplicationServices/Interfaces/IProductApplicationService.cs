5919814372865da3defcf1a9add1dd575f720f63
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-08 20:36:10 -0300

    feature: add application service generic search

--- src/StorEsc.ApplicationServices/Interfaces/IProductApplicationService.cs
+++ src/StorEsc.ApplicationServices/Interfaces/IProductApplicationService.cs
@@ -8,7 +8,12 @@ public interface IProductApplicationService
     Task<IList<ProductDto>> GetSellerProductsAsync(string sellerId);
     Task<Optional<ProductDto>> CreateProductAsync(ProductDto productDto);
     Task<IList<ProductDto>> GetLastProductsAsync();
-    Task<IList<ProductDto>> SearchProductsByName(string name);
+    Task<IList<ProductDto>> SearchProductsAsync(
+        string sellerId,
+        string name,
+        string description,
+        decimal minimumPrice = 0,
+        decimal maximumPrice = Decimal.MaxValue);
     Task<Optional<ProductDto>> UpdateProductAsync(string productId, string sellerId, ProductDto productDto);
     Task<bool> DisableProductAsync(string productId, string sellerId);
     Task<bool> EnableProductAsync(string productId, string sellerId);
