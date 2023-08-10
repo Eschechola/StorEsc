ec08ffc191b64962d83e9d21d7bb57d3ab6e0b33
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-10 16:19:24 -0300

    feature: apply full text search on description field and order by name in search

--- src/StorEsc.Infrastructure/Interfaces/Repositories/IProductRepository.cs
+++ src/StorEsc.Infrastructure/Interfaces/Repositories/IProductRepository.cs
@@ -5,9 +5,8 @@ namespace StorEsc.Infrastructure.Interfaces.Repositories;
 public interface IProductRepository : IRepository<Product>
 {
     Task<IList<Product>> SearchProductsAsync(
-        string sellerId,
-        string name,
-        string description,
+        string sellerId = "",
+        string name = "",
         decimal minimumPrice = 0,
-        decimal maximumPrice = Decimal.MaxValue);
+        decimal maximumPrice = 1_000_000);
 }
--- src/StorEsc.Infrastructure/Repositories/ProductRepository.cs
+++ src/StorEsc.Infrastructure/Repositories/ProductRepository.cs
@@ -19,20 +19,19 @@ public class ProductRepository : Repository<Product>, IProductRepository
     public override IUnitOfWork UnitOfWork => _context;
 
     public async Task<IList<Product>> SearchProductsAsync(
-        string sellerId,
-        string name,
-        string description,
+        string sellerId = "",
+        string name = "",
         decimal minimumPrice = 0,
-        decimal maximumPrice = Decimal.MaxValue)
+        decimal maximumPrice = 1_000_000)
     {
         var query = _dbSet as IQueryable<Product>;
 
         query = FilterProductsBySellerId(query, sellerId);
-        query = FilterProductsByName(query, name);
-        query = FilterProductsByDescription(query, description);
+        query = FilterProductsByNameOrDescription(query, name);
         query = FilterProductsByPrice(query, minimumPrice, maximumPrice);
 
         return await query.AsNoTracking()
+            .OrderBy(entity => entity.Name)
             .ToListAsync();
     }
 
@@ -41,16 +40,11 @@ public class ProductRepository : Repository<Product>, IProductRepository
             ? query
             : query.Where(entity => entity.SellerId.Equals(Guid.Parse(sellerId)));
     
-    private IQueryable<Product> FilterProductsByName(IQueryable<Product> query, string name)
+    private IQueryable<Product> FilterProductsByNameOrDescription(IQueryable<Product> query, string name)
         => string.IsNullOrEmpty(name)
             ? query
-            : query.Where(entity => Functions.FreeText(entity.Name, name));
+            : query.Where(entity => Functions.FreeText(entity.Name, name) || Functions.FreeText(entity.Description, name));
     
-    private IQueryable<Product> FilterProductsByDescription(IQueryable<Product> query, string description)
-        => string.IsNullOrEmpty(description)
-            ? query
-            : query.Where(entity => Functions.FreeText(entity.Description, description));
-
     private IQueryable<Product> FilterProductsByPrice(
         IQueryable<Product> query,
         decimal minimumPrice = 0,
