d730e603493eb356dfe5293680213f9efa3a22ad
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-10 16:48:12 -0300

    feature: add orderBy to search domain service

--- src/StorEsc.DomainServices/Interfaces/IProductDomainService.cs
+++ src/StorEsc.DomainServices/Interfaces/IProductDomainService.cs
@@ -1,4 +1,5 @@
 using StorEsc.Core.Data.Structs;
+using StorEsc.Core.Enums;
 using StorEsc.Domain.Entities;
 
 namespace StorEsc.DomainServices.Interfaces;
@@ -7,12 +8,13 @@ public interface IProductDomainService
 {
     Task<IList<Product>> GetLastProductsAsync();
     Task<Optional<Product>> CreateProductAsync(Product product);
+    Task<Optional<Product>> UpdateProductAsync(string productId, string sellerId, Product productUpdated);
+    Task<bool> DisableProductAsync(string productId, string sellerId);
+    Task<bool> EnableProductAsync(string productId, string sellerId);
     Task<IList<Product>> SearchProductsAsync(
         string sellerId = "",
         string name = "",
         decimal minimumPrice = 0,
-        decimal maximumPrice = 1_000_000);
-    Task<Optional<Product>> UpdateProductAsync(string productId, string sellerId, Product productUpdated);
-    Task<bool> DisableProductAsync(string productId, string sellerId);
-    Task<bool> EnableProductAsync(string productId, string sellerId);
+        decimal maximumPrice = 1_000_000,
+        OrderBy orderBy = OrderBy.CreatedAtDescending);
 }
