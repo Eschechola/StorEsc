ad16e2a144823587a3511d585d396817ec18984f
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-11 12:34:03 -0300

    feature: add my vouchers endpoint

---
+++ src/StorEsc.Api/Controllers/V1/VoucherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StorEsc.API.Token;
+using StorEsc.API.Token.Extensions;
+
+namespace StorEsc.API.Controllers;
+
+[Route("api/v1/[controller]")]
+public class VoucherController : BaseController
+{
+    [Route("my-vouchers")]
+    [Authorize(Roles = Roles.Seller)]
+    public async Task<IActionResult> GetVouchersAsync()
+    {
+        var sellerId = User.GetId();
+        
+        if (HasNotifications())
+            return Result();
+        
+        return Ok();
+    }
+}
