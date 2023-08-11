33654cbbb87ffaafc16c02a715e22693e5d7935b
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-11 12:33:45 -0300

    feature: add sellerId and enabled to voucher entity

--- src/StorEsc.Domain/Entities/Voucher.cs
+++ src/StorEsc.Domain/Entities/Voucher.cs
@@ -5,64 +5,43 @@ namespace StorEsc.Domain.Entities;
 
 public class Voucher : Entity, IAggregateRoot
 {
+    public Guid SellerId { get; private set; }
     public string Code { get; private set; }
     public decimal? ValueDiscount { get; private set; }
     public decimal? PercentageDiscount { get; private set; }
     public bool IsPercentageDiscount { get; private set; }
+    public bool Enabled { get; private set; }
     
     //EF
     public List<Order> Orders { get; private set; }
+    public Seller Seller { get; private set; }
 
     protected Voucher() { }
     
     public Voucher(
-        string code,
-        decimal? valueDiscount,
-        decimal? percentageDiscount,
-        bool isPercentageDiscount)
-    {
-        Code = code;
-        ValueDiscount = valueDiscount;
-        PercentageDiscount = percentageDiscount;
-        IsPercentageDiscount = isPercentageDiscount;
-        
-        Validate();
-    }
-
-    public Voucher(
-        Guid id,
-        string code,
-        decimal? valueDiscount,
-        decimal? percentageDiscount,
-        bool isPercentageDiscount) 
-        : base(id)
-    {
-        Code = code;
-        ValueDiscount = valueDiscount;
-        PercentageDiscount = percentageDiscount;
-        IsPercentageDiscount = isPercentageDiscount;
-        
-        Validate();
-    }
-    
-    public Voucher(
-        Guid id,
+        Guid sellerId,
         string code,
         decimal? valueDiscount,
         decimal? percentageDiscount,
         bool isPercentageDiscount,
-        List<Order> orders) 
-        : base(id)
+        bool enabled)
     {
+        SellerId = sellerId;
         Code = code;
         ValueDiscount = valueDiscount;
         PercentageDiscount = percentageDiscount;
         IsPercentageDiscount = isPercentageDiscount;
-        Orders = orders;
+        Enabled = enabled;
         
         Validate();
     }
-
+    
     public void Validate()
         => base.Validate(new VoucherValidator(), this);
+
+    public void Enable()
+        => Enabled = true;
+
+    public void Disable()
+        => Enabled = false;
 }
