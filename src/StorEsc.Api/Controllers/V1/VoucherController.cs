28ebb605e0c2511e7e4fa2e7c419b13edbf76ec1
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-14 16:02:19 -0300

    feature: add seller vouchers endpoint

--- src/StorEsc.Api/Controllers/V1/VoucherController.cs
+++ src/StorEsc.Api/Controllers/V1/VoucherController.cs
@@ -1,22 +1,47 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StorEsc.API.Token;
 using StorEsc.API.Token.Extensions;
+using StorEsc.API.ViewModels;
+using StorEsc.Application.Interfaces;
+using StorEsc.Core.Communication.Mediator.Notifications;
 
 namespace StorEsc.API.Controllers;
 
 [Route("api/v1/[controller]")]
 public class VoucherController : BaseController
 {
+    private readonly IVoucherApplicationService _voucherApplicationService;
+    
+    public VoucherController(
+        IVoucherApplicationService voucherApplicationService,
+        INotificationHandler<DomainNotification> domainNotificationHandler) : base(domainNotificationHandler)
+    {
+        _voucherApplicationService = voucherApplicationService;
+    }
+    
+    [HttpGet]
     [Route("my-vouchers")]
     [Authorize(Roles = Roles.Seller)]
     public async Task<IActionResult> GetVouchersAsync()
     {
         var sellerId = User.GetId();
+        var vouchers = await _voucherApplicationService.GetSellerVouchersAsync(sellerId);
         
         if (HasNotifications())
             return Result();
+
+        if (vouchers.Any())
+            return Ok(new ResultViewModel
+            {
+                Message = "Vouchers found with success",
+                Success = true,
+                Data = vouchers
+            });
         
-        return Ok();
+        return NoContent();
     }
+    
+
 }
