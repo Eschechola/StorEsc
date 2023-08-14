8b5cb2d60b293d3096c41d9094370665854bb0d8
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-14 16:01:16 -0300

    feature: add voucher repository

---
+++ src/StorEsc.Infrastructure/Repositories/VoucherRepository.cs
@@ -0,0 +1,18 @@
+﻿using StorEsc.Domain.Entities;
+using StorEsc.Infrastructure.Context;
+using StorEsc.Infrastructure.Interfaces;
+using StorEsc.Infrastructure.Interfaces.Repositories;
+
+namespace StorEsc.Infrastructure.Repositories;
+
+public class VoucherRepository : Repository<Voucher>, IVoucherRepository
+{
+    private readonly StorEscContext _context;
+
+    public VoucherRepository(StorEscContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public override IUnitOfWork UnitOfWork => _context;
+}
