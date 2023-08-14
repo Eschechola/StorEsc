4218227840215510a7fc2c641b765f9fc6bd052d
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-14 16:01:31 -0300

    feature: add voucher application service

---
+++ src/StorEsc.ApplicationServices/Services/VoucherApplicationService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using StorEsc.Application.Dtos;
+using StorEsc.Application.Interfaces;
+using StorEsc.DomainServices.Interfaces;
+
+namespace StorEsc.Application.Services;
+
+public class VoucherApplicationService : IVoucherApplicationService
+{
+    private readonly IMapper _mapper;
+    private readonly IVoucherDomainService _voucherDomainService;
+
+    public VoucherApplicationService(
+        IVoucherDomainService voucherDomainService,
+        IMapper mapper)
+    {
+        _voucherDomainService = voucherDomainService;
+        _mapper = mapper;
+    }
+
+    public async Task<IList<VoucherDto>> GetSellerVouchersAsync(string sellerId)
+    {
+        var vouchers = await _voucherDomainService.GetSellerVouchersAsync(sellerId);
+
+        return _mapper.Map<IList<VoucherDto>>(vouchers);
+    }
+}
