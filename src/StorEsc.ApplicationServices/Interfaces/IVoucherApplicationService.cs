088464030a834cf90709cca528d518f0c5bd7369
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-08-17 22:09:41 -0300

    feature: add create voucher to application services

--- src/StorEsc.ApplicationServices/Interfaces/IVoucherApplicationService.cs
+++ src/StorEsc.ApplicationServices/Interfaces/IVoucherApplicationService.cs
@@ -1,8 +1,10 @@
 ﻿using StorEsc.Application.Dtos;
+using StorEsc.Core.Data.Structs;
 
 namespace StorEsc.Application.Interfaces;
 
 public interface IVoucherApplicationService
 {
     Task<IList<VoucherDto>> GetSellerVouchersAsync(string sellerId);
+    Task<Optional<VoucherDto>> CreateVoucherAsync(string sellerId, VoucherDto voucherDto);
 }
