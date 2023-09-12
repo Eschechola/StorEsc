ae0afed9592c7f777316eda1b6562727149afc6b
Lucas Eschechola <lucas.eschechola@outlook.com>
2023-09-12 13:56:24 -0300

    test: add voucher faker

---
+++ src/StorEsc.Tests/Fakers/Entities/VoucherFaker.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using StorEsc.Domain.Entities;
+
+namespace StorEsc.Tests.Fakers.Entities;
+
+public class VoucherFaker: BaseFaker<Voucher>
+{
+    private readonly Randomizer _randomizer = new();
+
+    public override Voucher GetValid()
+        => new Voucher(
+            sellerId: Guid.NewGuid(),
+            code: _randomizer.String2(3, 80).ToUpper(),
+            valueDiscount: _randomizer.Decimal(1, 1_000_000),
+            percentageDiscount: _randomizer.Decimal(0, 100),
+            isPercentageDiscount: true,
+            enabled: true
+        );
+
+    public override Voucher GetInvalid()
+        => new Voucher(
+            sellerId: Guid.Empty,
+            code: "",
+            valueDiscount: 0,
+            percentageDiscount: 0,
+            isPercentageDiscount: true,
+            enabled: false
+        );
+}
